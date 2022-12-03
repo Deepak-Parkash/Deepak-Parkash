c68b9303ec24f5b91a5dd1d7e2bc7fcda9419182
Wjis333@live.rhul.ac.uk <wkis175@live.rhul.ac.uk>
2022-12-03 14:03:35 -0800

    updated files

--- deepak_calc/calc_d/deepack/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.junit.jupiter.api;
-
-
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
-}
